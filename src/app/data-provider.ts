4b624d39ad2b0515e74ad013643bab48aa0bb030
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-18 23:06:17 +0400

    feat: initialize data provider

---
+++ src/app/data-provider.ts
@@ -0,0 +1,10 @@
+export const dataProvider = {
+  login: async (email: string, password: string) => {},
+  register: async (
+    avatar: string | BinaryType,
+    email: string,
+    password: string,
+    password_confirmation: string,
+    username: string
+  ) => {},
+};
