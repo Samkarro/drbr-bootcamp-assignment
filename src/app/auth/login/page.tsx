ab95d491917453ba38fcf3109382c9a007ec4e0e
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-18 22:36:00 +0400

    feat: auth page skeletons
    feat: login & register page skeletons. Removing excess files temporarily

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,1 +1,26 @@
-export default function Login() {}
+export default function Login() {
+  return (
+    <div className="auth-page-container">
+      <h1>Log in</h1>
+      <form>
+        <input
+          className="email"
+          // value="email"
+          placeholder="Email"
+          required
+        ></input>
+        <input
+          className="password"
+          // value="password"
+          placeholder="Password"
+          type="password"
+          required
+        ></input>
+        <button type="submit">Log in</button>
+        <p>
+          Not a member? <a href="/auth/register">Register</a>
+        </p>
+      </form>
+    </div>
+  );
+}
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,1 +1,42 @@
-export default function Register() {}
+export default function Register() {
+  return (
+    <div className="auth-page-container">
+      <h1>Register</h1>
+      <h2>
+        <i>(Add an image upload input)</i> {/* placeholder */}
+      </h2>
+      <form>
+        <input
+          className="username"
+          //   value="username"
+          placeholder="Username"
+          required
+        ></input>
+        <input
+          className="email"
+          //   value="email"
+          placeholder="Email"
+          required
+        ></input>
+        <input
+          className="password"
+          //   value="password"
+          placeholder="Password"
+          type="password"
+          required
+        ></input>
+        <input
+          className="password-confirmation"
+          //   value="password-confirmation"
+          placeholder="Confirm password"
+          type="password"
+          required
+        ></input>
+        <button type="submit">Register</button>
+        <p>
+          Already a member? <a href="/auth/login">Log in</a>
+        </p>
+      </form>
+    </div>
+  );
+}
