dcb97e29f5964d104f67cc769b0bdac3a9747109
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-19 15:05:09 +0400

    feat: connected to back-end

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { dataProvider } from "@/app/data-provider";
 import { useState } from "react";
 
 export default function Login() {
@@ -9,25 +10,26 @@ export default function Login() {
   return (
     <div className="auth-page-container">
       <h1>Log in</h1>
-      <form>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          className="email"
-          placeholder="Email"
-          required
-        ></input>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          className="password"
-          placeholder="Password"
-          type="password"
-          required
-        ></input>
-        <button type="submit">Log in</button>
-        <p>
-          Not a member? <a href="/auth/register">Register</a>
-        </p>
-      </form>
+
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        className="email"
+        placeholder="Email"
+        required
+      ></input>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        className="password"
+        placeholder="Password"
+        type="password"
+        required
+      ></input>
+      <button onClick={(e: any) => dataProvider.login(email, password)}>
+        Log in
+      </button>
+      <p>
+        Not a member? <a href="/auth/register">Register</a>
+      </p>
     </div>
   );
 }
--- src/app/data-provider.ts
+++ src/app/data-provider.ts
@@ -1,6 +1,27 @@
 export const dataProvider = {
   login: async (email: string, password: string) => {
-    return "test";
+    const formData = JSON.stringify({ email, password });
+    console.log(formData);
+    const response = await fetch(
+      "https://api.redseam.redberryinternship.ge/api/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: formData,
+      }
+    )
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        localStorage.setItem("username", data.user.username);
+        localStorage.setItem("email", data.user.username);
+        localStorage.setItem("token", data.token);
+      });
+    console.log(response);
   },
   register: async (
     avatar: string | null | BinaryType,
@@ -9,13 +30,26 @@ export const dataProvider = {
     password_confirmation: string,
     username: string
   ) => {
-    const formData = {
+    const formData = JSON.stringify({
       avatar,
       email,
       password,
       password_confirmation,
       username,
-    };
+    });
     console.log(formData);
+
+    const response = await fetch(
+      "https://api.redseam.redberryinternship.ge/api/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: formData,
+      }
+    );
+    console.log(response.body);
   },
 };
