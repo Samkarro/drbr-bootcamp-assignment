268d4b7919e8ae83f7c2ded74abc958c1d9a602b
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-19 14:18:01 +0400

    feat: refined file upload input

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,52 +1,73 @@
 "use client";
 
-import { useState } from "react";
+import { dataProvider } from "@/app/data-provider";
+import { FormEvent, useState } from "react";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [avatar, setAvatar] = useState(null);
   // Don't forget to add image state management
+
+  const handleFileChange = (e: any) => {
+    if (e !== null) {
+      setAvatar(e.target.files[0]);
+    }
+  };
 
   return (
     <div className="auth-page-container">
       <h1>Register</h1>
-      <h2>
-        <i>(Add an image upload input)</i> {/* placeholder */}
-      </h2>
-      <form>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          className="username"
-          placeholder="Username"
-          required
-        ></input>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          className="email"
-          placeholder="Email"
-          required
-        ></input>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          className="password"
-          placeholder="Password"
-          type="password"
-          required
-        ></input>
-        <input
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-          className="password-confirmation"
-          placeholder="Confirm password"
-          type="password"
-          required
-        ></input>
-        <button type="submit">Register</button>
-        <p>
-          Already a member? <a href="/auth/login">Log in</a>
-        </p>
-      </form>
+      <input
+        className="user-image-upload"
+        type="file"
+        onChange={handleFileChange}
+        accept="image/png, image/jpeg, image/jpg"
+      />
+      <input
+        onChange={(e) => setUsername(e.target.value)}
+        className="username"
+        placeholder="Username"
+        required
+      ></input>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        className="email"
+        placeholder="Email"
+        required
+      ></input>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        className="password"
+        placeholder="Password"
+        type="password"
+        required
+      ></input>
+      <input
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+        className="password-confirmation"
+        placeholder="Confirm password"
+        type="password"
+        required
+      ></input>
+      <button
+        onClick={(e) =>
+          dataProvider.register(
+            avatar,
+            email,
+            password,
+            passwordConfirmation,
+            username
+          )
+        }
+      >
+        Register
+      </button>
+      <p>
+        Already a member? <a href="/auth/login">Log in</a>
+      </p>
     </div>
   );
 }
--- src/app/data-provider.ts
+++ src/app/data-provider.ts
@@ -3,10 +3,19 @@ export const dataProvider = {
     return "test";
   },
   register: async (
-    avatar: string | BinaryType,
+    avatar: string | null | BinaryType,
     email: string,
     password: string,
     password_confirmation: string,
     username: string
-  ) => {},
+  ) => {
+    const formData = {
+      avatar,
+      email,
+      password,
+      password_confirmation,
+      username,
+    };
+    console.log(formData);
+  },
 };
