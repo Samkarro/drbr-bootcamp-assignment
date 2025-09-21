8e5f5f80752a4751ac05e401ec88956099f191e6
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-21 15:22:57 +0400

    feat: aligned image, applied form margins

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -11,32 +11,42 @@ export default function Login() {
 
   return (
     <div className="auth-page-container">
-      <AuthHeader></AuthHeader>
-      <div className="auth-page-form-container">
-        <h1 style={{ width: "544px" }}>Log in</h1>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          className="auth-text-input email"
-          placeholder="Email"
-          required
-        ></input>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          className="auth-text-input password"
-          placeholder="Password *"
-          type="password"
-          required
-        ></input>
-        <button
-          className="cta-button"
-          onClick={(e: any) => dataProvider.login(email, password)}
-        >
-          Log in
-        </button>
-        <p>
-          Not a member? <a href="/auth/register">Register</a>
-        </p>
-      </div>
+      <AuthHeader />
+      <main className="auth-main">
+        <img
+          alt="test"
+          className="auth-page-image"
+          src={"/images/authpage.png"}
+        />
+        <div className="auth-page-form-container">
+          <h1 style={{ width: "544px", marginBottom: "48px" }}>Log in</h1>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="auth-text-input email"
+            placeholder="Email"
+            required
+          ></input>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="auth-text-input password"
+            placeholder="Password *"
+            type="password"
+            required
+          ></input>
+          <button
+            className="cta-button"
+            onClick={(e: any) => dataProvider.login(email, password)}
+            style={{
+              margin: "22px 0px 24px 0px",
+            }}
+          >
+            Log in
+          </button>
+          <p>
+            Not a member? <a href="/auth/register">Register</a>
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
