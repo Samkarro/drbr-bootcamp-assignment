66f443ff1dcac9d7895f7ec0b5d02d1ccf9ceb22
Luka Gogichaishvili <luka595664@gmail.com>
2025-09-22 14:54:41 +0400

    feat: init products page
    Style products filter ribbon

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,1 +1,15 @@
-export default function Products() {}
+import MainHeader from "../../../components/header-main";
+import "./styles.products.css";
+
+export default function Products() {
+  return (
+    <div>
+      <MainHeader />
+      <main className="products-main">
+        <div className="product-filter-bar-container">
+          <h1>Products</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
